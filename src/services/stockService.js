413f4b8388d50d230bdfb823f782f4cb870a80b5
ikicamilo <ikicamilo@gmail.com>
2024-11-23 17:29:44 -0500

    Explanatory manual is completed

--- src/services/stockService.js
+++ src/services/stockService.js
@@ -17,22 +17,24 @@ const getStockProductoSede = async (codProd, codSed) => {
     throw new Error("La sede no existe");
   }
 
-  return await Stock.findOne({
-    where: { codProd, codSed },
-    include: [
-      {
-        model: Producto,
-        as: "productos",
-        attributes: ["nomProd"],
-      },
-      {
-        model: Sede,
-        as: "sedes",
-        attributes: ["nomSed"],
-      },
-    ],
-    order: [["updatedAt", "DESC"]],
-  });
+  return (
+    (await Stock.findOne({
+      where: { codProd, codSed },
+      include: [
+        {
+          model: Producto,
+          as: "productos",
+          attributes: ["nomProd"],
+        },
+        {
+          model: Sede,
+          as: "sedes",
+          attributes: ["nomSed"],
+        },
+      ],
+      order: [["updatedAt", "DESC"]],
+    })) ?? []
+  );
 };
 
 module.exports = {
